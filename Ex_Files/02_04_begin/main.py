bad3eab7a8fb4a5f86a2e25e4d0216f400b03af2
Thomas John <thomasvjhn@gmail.com>
2023-10-12 10:19:46 +0100

    Update

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -1,21 +1,29 @@
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
+print("While Loop")
 i = 0
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
 
+print("For Loop")
 for name in NAMES:
     print(name)
 
+print("For Loop with Zip")
 for name, age in zip(NAMES, AGES):
     print(f"{name} {age}")
 
+print("For Loop with Reversed")
 for name in reversed(NAMES):
     print(name)
 
+print("For Loop with Index")
 for i in range(5):
     print(i)
 
 # enumerate
+print("For Loop with Enumerate")
+for i, name in enumerate(NAMES):
+    print(f"{i} {name}")
